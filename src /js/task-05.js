f757222b2b93c742e7f99d4d7b632fe30162000a
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-09-26 00:59:13 +0200

    l

--- src /js/task-05.js
+++
@@ -1,6 +0,0 @@
-const nameInput = document.querySelector(`#name-input`);
-const nameOutput = document.querySelector(`#name-output`);
-
-nameInput.addEventListener("input", (event) => {
-nameOutput.textContent = event.target.value||`Anonymous`;
-});
--- src /js/task-07.js
+++
@@ -1,7 +0,0 @@
-const fontSizeControl = document.querySelector(`#font-size-control`);
-const textElement = document.querySelector(`#text`);
-
-fontSizeControl.addEventListener("input", (event) => {
-    const fontSize = fontSizeControl.value;
-    textElement.style.fontSize = `${fontSize}px`;
-});
