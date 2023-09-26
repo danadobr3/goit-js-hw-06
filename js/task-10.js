bcb98b9e9f08b30cf65bbc39df51fe3c63ebab7f
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-09-26 22:23:44 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,40 +1,52 @@
-const controlsDiv = document.querySelector('#controls');
-const input = controlsDiv.querySelector('input');
-const createButton = controlsDiv.querySelector('[data-create]');
-const destroyButton = controlsDiv.querySelector('[data-destroy]');
-const boxesDiv = document.querySelector('#boxes');
 
-createButton.addEventListener('click', () => createBoxes(input.value));
-destroyButton.addEventListener('click', destroyBoxes);
+
+
+const selectors = {
+  input: document.querySelector(".js-input"),
+  create: document.querySelector(".js-create"),
+  destroy: document.querySelector(".js-destroy"),
+  boxes: document.querySelector('#boxes')
+};
+selectors.create.addEventListener("click", handlerCreate);
+selectors.destroy.addEventListener("click", handlerDestroy);
+
+function handlerCreate() {
+  const amount = Number(selectors.input.value);
+  createBoxes(amount);
+  selectors.input.value = 0
+}
+
+function handlerDestroy() {
+  selectors.boxes.innerHTML = ''
+}
 
 function createBoxes(amount) {
-  amount = parseInt(amount);
-  if (isNaN(amount) || amount < 1 || amount > 100) {
-    alert('Please enter a valid number between 1 and 100.');
-    return;
+  handlerDestroy()
+  let size = 20;
+  const elements = []
+ for (let i = 0; i < amount; i += 1) {
+    size += 10;
+
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor()
+    elements.push(div)
   }
-
-  boxesDiv.innerHTML = '';
-  const fragment = document.createDocumentFragment();
-
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
-    const size = 30 + i * 10;
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    fragment.appendChild(box);
-  }
-
-  boxesDiv.appendChild(fragment);
+selectors.boxes.append(...elements)
 }
 
-function destroyBoxes() {
-  boxesDiv.innerHTML = '';
-}
 
+
+
+
+function handlerDestroy() {
+  selectors.boxes.innerHTML = ''
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, '0')}`;
+    .padStart(6, 0)}`;
 }
+
+
